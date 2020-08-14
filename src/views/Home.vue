e0b064454426221c8729b12e1728d7ce2d00aa3c
Desire Muson <desiremusond@gmail.com>
2020-08-14 15:28:08 +0200

    selection option

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,12 @@
 <template>
   <v-data-table
     v-model="selected"
+    show-select
     :headers="headers"
     :items="degrees"
-    :single-select="singleSelect"
-    item-key="name"
-    show-select
-    class="elevation-1"
+    item-key="code"
+    class="elevation-1 pa-6"
   >
-    <template v-slot:top>
-      <v-switch
-        v-model="singleSelect"
-        label="Single select"
-        class="pa-3"
-      ></v-switch>
-    </template>
   </v-data-table>
 </template>
 
@@ -27,20 +19,9 @@ export default {
   data () {
     return {
       degrees: [],
-      singleSelect: false,
-      selected: [],
-      headers: [
-        {
-          text: 'Course Name',
-          align: 'left',
-          sortable: false,
-          value: 'name'
 
-        },
-        { text: 'Level', value: 'level' },
-        { text: 'Duration', value: 'duration' },
-        { text: 'Code', value: 'code' }
-      ]
+      singleSelect: false,
+      selected: []
 
     }
   },
@@ -51,6 +32,29 @@ export default {
         this.degrees = response[0].data.concat(response[1].data)
       })
       .catch(err => console.log(err))
+  },
+  computed: {
+    headers () {
+      return [
+        {
+          text: 'Course Name',
+          align: 'left',
+          sortable: false,
+          value: 'name',
+
+          filter: this.courseFilter
+        },
+        {
+          text: 'Level',
+          value: 'level',
+          filter: this.qualificationFilter
+        },
+        { text: 'Duration', value: 'duration' },
+        { text: 'Code', value: 'code' }
+      ]
+    }
+
   }
+
 }
 </script>
